2ce2159d273256dc877530b52d651e7c3b018a3f
Pradeep Bugga <pradeep.bugga0@gmail.com>
2026-07-31 15:16:30 -0400

    fix typo

--- core/extraction/registry.py
+++ core/extraction/registry.py
@@ -1,6 +1,6 @@
 from .extractors.sponsor_attestation import extract_sponsor_attestation
 from .extractors.registry_extract import extract_registry_extract
-from .extractors.adjudication_note import extract_adjudication_note
+from .extractors.adjudicator_note import extract_adjudicator_note
 from .extractors.intake_form import extract_intake_form
 from .extractors.biometric_slip import extract_biometric_slip
 from .extractors.fee_receipt import extract_fee_receipt
@@ -9,7 +9,7 @@ from .extractors.unknown import extract_unknown
 EXTRACTORS = {
     "sponsor_attestation": extract_sponsor_attestation,
     "registry_extract": extract_registry_extract,
-    "adjudication_note": extract_adjudication_note,
+    "adjudicator_note": extract_adjudicator_note,
     "intake_form": extract_intake_form,
     "biometric_slip": extract_biometric_slip,
     "fee_receipt": extract_fee_receipt,
